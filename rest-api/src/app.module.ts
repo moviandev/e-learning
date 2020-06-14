3c025def67efcbfacebcfb0f63a70f48e9c3d406
Matheus Viana <matheus_o_viana@hotmail.com>
2020-06-14 19:36:01 -0300

    feat(app.module): added mongoose setup

--- rest-api/src/app.module.ts
+++ rest-api/src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { CoursesModule } from './courses/courses.module';
+import { MONGODB_CONNECTION } from './courses/constants';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [
+    CoursesModule,
+    MongooseModule.forRoot(MONGODB_CONNECTION, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+  ],
 })
 export class AppModule {}
