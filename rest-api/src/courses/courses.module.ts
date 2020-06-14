1d66da1573bf8286a4557bf9d0e409473692a8d2
Matheus Viana <matheus_o_viana@hotmail.com>
2020-06-14 19:37:57 -0300

    feat(courses.module): added mongoose for feature

--- rest-api/src/courses/courses.module.ts
+++ rest-api/src/courses/courses.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { CoursesControllers } from './controllers/courses.controller';
+import { CoursesSchema } from './schemas/courses.schema';
+import { CourseRepository } from './repositories/courses.repository';
 
 @Module({
   controllers: [CoursesControllers],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Course',
+        schema: CoursesSchema,
+      },
+    ]),
+  ],
+  providers: [CourseRepository],
 })
 export class CoursesModule {}
